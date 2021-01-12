40380e099a97624ba9face0ef6c2159883a106c9
itoma1124 <alpineski100@gmail.com>
2021-01-12 18:31:49 +0900

    item controller update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only:[:new,:create,:edit,:update]
-  before_action @item = Item.find(params[:id])
+  before_action :set_item, only: [:edit, :show]
   before_action :unmach_user, only: [:edit,:update]
 
   def index
@@ -48,5 +48,9 @@ class ItemsController < ApplicationController
     unless current_user.id == @item.user_id
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
