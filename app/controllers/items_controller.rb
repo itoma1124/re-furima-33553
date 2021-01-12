df51d0af808e47162831d5f28334bbcc8ff3be66
itoma1124 <alpineski100@gmail.com>
2021-01-12 18:53:59 +0900

    item controller update (4th)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only:[:new,:create,:edit,:update]
+  before_action :authenticate_user!, except:[:index,:show]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :unmach_user, only: [:edit,:update]
 
@@ -51,5 +51,5 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
-  
+
 end
