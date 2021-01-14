5575c549e5cd3473fdd7b8e18da26527c5b46818
itoma1124 <alpineski100@gmail.com>
2021-01-14 19:29:57 +0900

    フォームモデルの設定
    １つのフォームから複数のテーブルにデータを送信できるようにした

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,30 +1,24 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
-  end
-
-  def new
+    @order_address = OrderAddress.new
   end
 
   def create
-    @order_address = OrderAddress.create(order_params)
+    @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       @order_address.save
-      redirect_to action: :index
+      redirect_to root_path
     else
-      render action: :new
+      render action: :index
     end
   end
 
   private
 
-  def order_params
+  def order_address_params
     @item = Item.find(params[:item_id])
-    params.merge(user_id: current_user.id, item_id:@item.id )
-  end
-
-  def address_params(order)
-    params.permit(:postal_code, :province_id, :city, :address_line, :building, :phone_num).merge(user_id: current_user.id, order_id: order.id)
+    params.require(:order_address).permit(:postal_code, :province_id, :city, :address_line, :building, :phone_num).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
 end
---
+++ app/models/order_address.rb
@@ -0,0 +1,23 @@
+class OrderAddress
+
+  include ActiveModel::Model
+
+  attr_accessor :postal_code,:province_id,:city,:address_line,:building,:phone_num,:user_id,:item_id,:token
+
+  with_options presence: true do
+    validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/, message: '半角数字を使用し,123-4567のように入力してください' }
+    validates :province_id , numericality: { other_than: 0 } 
+    validates :city,         format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :address_line
+    validates :phone_num,    format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください。ハイフン(-)は使用しないでください。' }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 住所の情報を保存
+    Address.create(postal_code: postal_code, province: province, city: city, address_line: address_line, building: building, order_id: order.id)
+  end
+end
