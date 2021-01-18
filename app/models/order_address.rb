40087e08acce7cc5e147227e9617d8c13504e320
itoma1124 <alpineski100@gmail.com>
2021-01-18 14:56:03 +0900

    エラー文修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,12 +4,12 @@ class OrderAddress
 
   attr_accessor :postal_code,:province_id,:city,:address_line,:building,:phone_num,:user_id,:item_id,:token
 
-  with_options presence: true do
-    validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/, message: '半角数字を使用し,123-4567のように入力してください' }
+  with_options presence: { message: "を選択してください。" } do
+    validates :postal_code,  format: { with: /\A\d{3}[-]\d{4}\z/, message: 'は半角数字を使用し,123-4567のように入力してください' }
     validates :province_id , numericality: { other_than: 0 } 
-    validates :city,         format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' }
+    validates :city,         format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: 'は全角文字を使用してください' }
     validates :address_line
-    validates :phone_num,    format: { with: /\A[0-9]{7,11}\z/, message: '11桁以内の半角数字を使用してください。ハイフン(-)は使用しないでください' }
+    validates :phone_num,    format: { with: /\A[0-9]{7,11}\z/, message: 'は11桁以内の半角数字を使用してください。ハイフン(-)は使用しないでください' }
     validates :token
     validates :user_id
     validates :item_id
