79935ebdcb2b489ac86e35f8b2628e997081933a
itoma1124 <alpineski100@gmail.com>
2021-01-19 17:59:12 +0900

    ページ切り替え実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,11 +12,11 @@ class Item < ApplicationRecord
   belongs_to :status
 
   def previous
-    user.items.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
+    Item.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
   end
 
   def next
-    user.items.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
+    Item.order('created_at desc, id desc').where('created_at >= ? and id > ?', created_at, id).reverse.first
   end
 
   # Validation
